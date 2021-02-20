bfd0582f1318971b6a5b2b0cd1c09b248d93e179
Erwan BASILE <erwan.basile@gmail.com>
2021-02-20 14:25:15 +0100

    ✨ Add test page

---
+++ src/views/Test.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>My name is : {{ name }}</h1>
+    <button @click="saveConfig()">Save Config !</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    saveConfig() {
+      window.ipc.invoke("save-config", { test: true });
+    },
+  },
+  data: function () {
+    return {
+      name: "Erwan",
+    };
+  },
+};
+</script>
+
+<style>
+</style>

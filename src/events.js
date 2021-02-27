a9af83b655610dfaee0aebf352e7c72cd0a0729c
Erwan BASILE <erwan.basile@gmail.com>
2021-02-28 00:02:13 +0100

    ✨ Register new events to use eletron dialogs

--- src/events.js
+++ src/events.js
@@ -1,4 +1,4 @@
-import { ipcMain } from 'electron'
+import { ipcMain, dialog } from 'electron'
 import { saveConfig, readConfig } from './config'
 
 export const registerEvents = () => {
@@ -11,6 +11,23 @@ export const registerEvents = () => {
     ipcMain.handle('read-config', (event, key) => {
         return readConfig(key);
     });
+
+    /**
+     * Windows events
+     */
+
+     /**
+      * Electron dialogs
+      * For a list of available option read :
+      * https://www.electronjs.org/docs/api/dialog
+      */
+    ipcMain.handle('messagebox', (event, options) => {
+        return dialog.showMessageBox(options)
+    })
+
+    ipcMain.handle('errorbox', (event, options) => {
+        return dialog.showErrorBox(options)
+    })
 
     /**
      * System value events

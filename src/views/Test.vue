08c7f1e8029b552e923c2524915796273f4a0917
Erwan BASILE <erwan.basile@gmail.com>
2021-02-27 22:43:31 +0100

    Update Test.vue test file

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
-    <h1>My name is : {{ name }}</h1>
+    <label for="key">Save in key:</label>
+    <input v-model="key" type="text" id="key" name="key" />
+    <label for="value">Save:</label>
+    <input v-model="value" type="text" id="value" name="value" />
     <button @click="saveConfig()">Save Config !</button>
+    <button @click="readConfig()">Read Config !</button>
   </div>
 </template>
 
@@ -9,12 +13,18 @@
 export default {
   methods: {
     saveConfig() {
-      window.ipc.invoke("save-config", { test: true });
+      window.ipc.invoke("save-config", this.key, this.value);
+    },
+    readConfig() {
+      //const keyFromMain = await 
+      window.ipc.invoke("read-config", this.key).then(e => console.log(e));
+      //console.log(keyFromMain);
     },
   },
   data: function () {
     return {
-      name: "Erwan",
+      key: "",
+      value: "",
     };
   },
 };

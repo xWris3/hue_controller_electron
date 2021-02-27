d937a08264b4b86b239741412eb620e61345cc43
Erwan BASILE <erwan.basile@gmail.com>
2021-02-27 22:40:15 +0100

    Update routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+//import Home from '../views/Home.vue'
+import Hue from '../views/Hue.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Hue',
+    component: Hue
   },
   {
     path: '/about',

a95981dbe89ee63e64cbf0d4abb566596a066712
Erwan BASILE <erwan.basile@gmail.com>
2021-02-28 14:25:08 +0100

    ✨ Add HueHub view

---
+++ src/views/Hue/HueHub.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      {{this.id}}
+      {{this.ip}}
+      {{this.username}}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: String,
+    ip: String,
+    username: String,
+  },
+};
+</script>
+
+<style>
+</style>

c8d69dcdba7840087702f053df09c1282b9cc288
Erwan BASILE <erwan.basile@gmail.com>
2021-03-14 16:37:16 +0100

    Add control for color temperature. Rework "setState" function around two "handleSuccess" and "handleError" functions.

--- src/components/Hue/HueLight.vue
+++ src/components/Hue/HueLight.vue
@@ -4,14 +4,41 @@
     <h1 class="text-center md:text-left text-xl font-semibold">
       {{ light.name }}
     </h1>
-    <div class="flex items-center justify-center mt-4">
+    <div class="flex flex-col items-center justify-center mt-4">
+      <span>Brightness :</span>
+      <round-slider
+        name="bri"
+        min="1"
+        max="254"
+        circleShape="pie"
+        rangeColor="rgb(243 172 12)"
+        startAngle="315"
+        radius="75%"
+        :editableTooltip="false"
+        :tooltipFormat="sliderPercentage"
+        :value="light.state.bri"
+        @input="(payload) => setState({ bri: payload })"
+      />
+      <span>Color temperature :</span>
+      <round-slider
+        name="bri"
+        :min="light.capabilities.control.ct.min"
+        :max="light.capabilities.control.ct.max"
+        circleShape="half-top"
+        startAngle="315"
+        radius="75%"
+        :editableTooltip="false"
+        :tooltipFormat="sliderPercentage"
+        :value="light.state.ct"
+        @input="(payload) => setState({ ct: payload })"
+      />
       <button
         @click="toggleLight()"
         :class="{
           'bg-red-500 hover:bg-red-700': light.state.on,
           'bg-green-500 hover:bg-green-700': !light.state.on,
         }"
-        class="text-white rounded px-1 mx-2"
+        class="text-white rounded w-2/3 mt-4 py-2"
       >
         <span v-if="light.state.on">Turn off</span>
         <span v-else>Turn on</span>
@@ -23,9 +50,13 @@
 
 <script>
 import axios from "axios";
+import RoundSlider from "vue-round-slider";
 
 export default {
   name: "HueLight",
+  components: {
+    RoundSlider,
+  },
   props: {
     apiId: String,
     uri: String,
@@ -72,33 +103,34 @@ export default {
       axios
         .put(`${this.baseApiUri}/${this.apiId}/state`, state)
         .then((response) => {
-          // handle success
-
-          // Read api feedback to verify if modifications were successful
-          let success = function () {
-            response.data.forEach((feedback) => {
-              if ("error" in feedback) {
-                return false;
-              }
-            });
-            return true;
-          };
-
-          if (success) {
-            //Change was accepted by the API
-            this.light.state = state;
-          } else {
-            //Change was rejected by the API
-            // TODO: Inform end-user
-          }
+          response.data.forEach((feedback) => {
+            if ("success" in feedback) {
+              this.handleStateSuccess(feedback.success);
+            } else {
+              this.handleStateError(feedback.error);
+            }
+          });
         })
         .catch((error) => {
           // handle error
           console.log(error);
         });
     },
+    handleStateError(error) {
+      // TODO: improve on that to handle error and relay api message
+      console.log(error);
+    },
+    handleStateSuccess(success) {
+      let key = Object.keys(success)[0].split("/").pop();
+      let value = Object.values(success)[0];
+
+      this.light.state[key] = value;
+    },
     toggleLight() {
       this.setState({ on: !this.light.state.on }); // send state to api
+    },
+    sliderPercentage: function (arg) {
+      return `${Math.round((arg.value / arg.options.max) * 100)}%`;
     },
   },
 };

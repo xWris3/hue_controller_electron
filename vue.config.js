b79378fc13d199e99d801fd8283745e30cbc1fcd
Erwan BASILE <erwan.basile@gmail.com>
2021-12-08 23:16:31 +0100

    Add author, repository, description and electron-builder publish script. (WIP) electron-updater dependency added.

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,10 @@ module.exports = {
     pluginOptions: {
         electronBuilder: {
             preload: 'src/preload.js',
+            builderOptions: {
+                "appId": "fr.ebasile.hue-controller",
+                "publish": ["github"], 
+              },
         }
     }
 }

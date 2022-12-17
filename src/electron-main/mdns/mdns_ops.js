3e29e2195320cc11932aa39a45e29d29ef7105fa
Erwan BASILE <erwan.basile@gmail.com>
2022-12-17 11:20:30 +0100

    Add mdns_ops.js responsible for every mdns-discovery module operations

---
+++ src/electron-main/mdns/mdns_ops.js
@@ -0,0 +1,24 @@
+// multicast dns module, see https://github.com/soef/mdns-discovery
+const Mdns = require('mdns-discovery');
+
+export const bridgesLookup = () => {
+    let mdns = new Mdns({
+        timeout: 3,
+        name: '_hue._tcp.local',
+        find: '_hue._tcp.local'
+    });
+
+    return new Promise((resolve, reject) => {
+        mdns.run((mDnsAnswers) => {
+            if (mDnsAnswers) {
+                var hubs = [];
+                mDnsAnswers.forEach(a => {
+                    hubs.push({ 'shortid': a.PTR.data.slice(14, 20), 'ip': a.ip });
+                });
+                return resolve(hubs);
+            }
+
+            return reject();
+        })
+    })
+}
